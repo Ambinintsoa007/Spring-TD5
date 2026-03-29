45b351b84ab1c28da6a35a650f18d99714eb54ed
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2026-03-29 08:30:33 +0300

    feat: adding DataSource for db connection

---
+++ src/main/java/org/demo/td5/config/DataSource.java
@@ -0,0 +1,29 @@
+package org.demo.td5.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataSource {
+
+    public Connection getConnection() {
+        try {
+            String jdbcUrl = System.getenv("JDBC_URL");
+            String user = System.getenv("USERNAME");
+            String password = System.getenv("PASSWORD");
+            return DriverManager.getConnection(jdbcUrl, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
